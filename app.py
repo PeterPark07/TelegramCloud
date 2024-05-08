05bf570a8725c3a27666eb5d9aaca0a3730b5946
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-05-08 07:42:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,24 @@ def telegram():
         bot.process_new_updates([update])
         return 'OK', 200
 
+# Handler for the '/start' command
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    response_text = "Hello! Welcome to this bot!\n\n"
+    response_text += "For help, use the command /help."
+    bot.reply_to(message, response_text)
+
+# Handler for the '/help' command
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    response_text = "Here are the available commands:\n\n"
+    response_text += "/start - Start the bot.\n"
+    response_text += "/help - Show this help message.\n"
+    bot.reply_to(message, response_text)
+
+
+
+
 
 
 
@@ -106,20 +124,7 @@ def upload_image():
 
 
 
-# Handler for the '/start' command
-@bot.message_handler(commands=['start'])
-def start_command(message):
-    response_text = "Hello! Welcome to this bot!\n\n"
-    response_text += "For help, use the command /help."
-    bot.reply_to(message, response_text)
 
-# Handler for the '/help' command
-@bot.message_handler(commands=['help'])
-def help_command(message):
-    response_text = "Here are the available commands:\n\n"
-    response_text += "/start - Start the bot.\n"
-    response_text += "/help - Show this help message.\n"
-    bot.reply_to(message, response_text)
 
 # Handler for document messages
 @bot.message_handler(content_types=['document'])
